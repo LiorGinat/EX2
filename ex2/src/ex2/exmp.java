30a67f090eee9b8ffd460962dc042bf8ef1d814b
liorg <liorg@DESKTOP-PCO15MN>
2026-03-27 17:54:55 +0300

    Pushed

--- ex2/src/ex2/exmp.java
+++ ex2/src/ex2/exmp.java
@@ -2,6 +2,6 @@ package ex2;
 
 public class exmp {
     public static void main(String[] args) {
-        System.out.println("push to origin!");
+        System.out.println("pushed to origin!");
     }
 }
